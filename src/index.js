01500504e62d5ac075505bedd7e69e7fd2e208f4
alexandre wagane <wagane1996@gmail.com>
2022-05-18 03:50:57 +0200

    React Redux Form

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-social/bootstrap-social.css";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Home from "./components/HomeComponent";
 import Main from "./components/MainComponent";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/HeaderComponent";
-import Footer from "./components/FooterComponent";
-import Menu from "./components/MenuComponent";
+import { BrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
 
